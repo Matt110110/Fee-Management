a2d409b6c65d2a40c0d263be5825a9855f46b213
Matt110110 <matruprasadchand@gmail.com>
2018-06-26 18:20:57 +0530

    Design of add student complete and start of backend implementation

--- src/AddStudent.java
+++ src/AddStudent.java
@@ -8,7 +8,17 @@ import java.io.IOException;
 @WebServlet(name = "AddStudent", urlPatterns = {"/AddStudent"})
 public class AddStudent extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        String fname = request.getParameter("fname");
+        String lname = request.getParameter("lname");
+        String email = request.getParameter("email");
+        char sex = request.getParameter("sex").charAt(0);
+        String course = request.getParameter("course");
+        int fees = Integer.parseInt(request.getParameter("fee"));
+        int paid = Integer.parseInt(request.getParameter("paid"));
+        int due = Integer.parseInt(request.getParameter("due"));
+        String address = request.getParameter("address");
+        String contact = request.getParameter("contact");
+        System.out.println(fname + lname + email + sex + course + fees + paid + due + address + contact);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
