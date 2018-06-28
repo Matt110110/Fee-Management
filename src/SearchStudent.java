1059ba89e241952427bd64448cd29ab5709c28c3
Matt110110 <matruprasadchand@gmail.com>
2018-06-28 15:55:53 +0530

    Design and backend implementation of search student complete

--- src/SearchStudent.java
+++ src/SearchStudent.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.SQLException;
 
 @WebServlet(name = "SearchStudent", urlPatterns = {"/SearchStudent"})
@@ -14,7 +15,50 @@ public class SearchStudent extends HttpServlet {
         s.setRollno(roll);
         try {
             s = DBUser.searchStudent(s);
-
+            if (DBUser.isCorrectRoll(s)) {
+                PrintWriter out = response.getWriter();
+                // TODO : Add a proper page with footers and headers
+                out.println("<html>");
+                out.println("<head>");
+                out.println("<title>Records</title>");
+                out.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"vendor/bootstrap/css/bootstrap.min.css\">");
+                out.println("<body>");
+                out.println("<div class='container'>");
+                out.println("<center>");
+                out.println("<h1>Student details :</h1>");
+                out.println("<p>Roll no. : " +s.getRollno()+ "</p>");
+                out.println("<p>Name : " +s.getName()+ "</p>");
+                out.println("<p>Email : "+s.getEmail()+"</p>");
+                out.println("<p>Sex : "+s.getSex()+"</p>");
+                out.println("<p>Course : "+s.getCourse()+"</p>");
+                out.println("<p>Fee : "+s.getFee()+"</p>");
+                out.println("<p>Paid : "+s.getPaid()+"</p>");
+                out.println("<p>Due : "+s.getDue()+"</p>");
+                out.println("<p>Address : "+s.getAddress()+"</p>");
+                out.println("<p>Contact : "+s.getContact()+"</p>");
+                out.println("<a href='searchStudent.html'><button class='btn-primary btn-lg'>Back</button></a>");
+                out.println("</center>");
+                out.println("</div>");
+                out.println("</body>");
+                out.println("</html>");
+            } else {
+                // TODO : Create proper pages to redirect.
+                PrintWriter out = response.getWriter();
+                out.println("<html>");
+                out.println("<head>");
+                out.println("<title>Error</title>");
+                out.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"vendor/bootstrap/css/bootstrap.min.css\">");
+                out.println("</head>");
+                out.println("<body>");
+                out.println("<div class='container'>");
+                out.println("<center>");
+                out.println("<h1 class=\"alert-danger\">No records could be found for the given Roll no.</h1>");
+                out.println("<a href='searchStudent.html'><button class='btn-danger btn-lg'>Return...</button></a>");
+                out.println("</center>");
+                out.println("</div>");
+                out.println("</body>");
+                out.println("</html>");
+            }
             // TODO : Remove debugging code and add proper code for printing Student details to the screen.
             System.out.println(s.getRollno() + s.getName() + s.getEmail() + s.getSex() + s.getCourse() + s.getFee() + s.getPaid() + s.getDue() + s.getAddress() + s.getContact());
         } catch (SQLException | ClassNotFoundException e) {
